d2b98b513c91702e19d3e4a9c749a451ac5e9849
AilsaLT <2926509946@qq.com>
2019-09-12 22:18:03 +0800

    在logo的布局上修改了scaleType属性

--- app/src/main/java/com/ghl/wuhan/secondhand/Logo_Activity.java
+++ app/src/main/java/com/ghl/wuhan/secondhand/Logo_Activity.java
@@ -1,9 +1,12 @@
 package com.ghl.wuhan.secondhand;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.widget.ImageView;
 
@@ -15,7 +18,11 @@ public class Logo_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.logo_activity);
-
+        if (Build.VERSION.SDK_INT >= 21){
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
         iv_logo = (ImageView) findViewById(R.id.iv_logo);
         startAlphaAnimation();
         myhandler.postDelayed(r, 3000);
